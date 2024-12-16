7ebe8c4393a2f746cb5521d4ef486263e90d092d
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-12-16 23:52:06 +1300

    hopefully updated to latest hc version

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from './src/main/defineConfig'
 
 export default defineConfig({
-  appId: 'org.visible.verification',
-  productName: 'Visible Verification',
-  version: '0.0.1',
+  appId: "org.visible-verification",
+  productName: "Visible Verification",
+  version: "0.0.1",
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,

35397932f8106746530159d24304b2b733567767
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-12-19 11:39:32 +1300

    add origin

--- src/main/holochainManager.ts
+++ src/main/holochainManager.ts
@@ -140,7 +140,7 @@ export class HolochainManager {
           const adminWebsocket = await AdminWebsocket.connect({
             url: new URL(`ws://127.0.0.1:${adminPort}`),
             wsClientOptions: {
-              origin: 'moss-admin-main',
+              origin: 'visible-verification',
             },
           })
           console.log('Connected to admin websocket.')

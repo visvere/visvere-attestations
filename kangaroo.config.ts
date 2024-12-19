241e5c299b9869b2ae2c67777c3c55b204597dce
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-12-19 23:29:06 +1300

    go to old original build

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from './src/main/defineConfig';
 
 export default defineConfig({
-  appId: 'org.holochain.kangaroo-electron',
-  productName: 'Holochain Kangaroo Electron',
-  version: '0.1.0',
+  appId: 'org.visible-verification',
+  productName: 'Visible Verification',
+  version: '0.0.2',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
